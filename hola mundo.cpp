6d96dabd5c2dbd9816b42570ce070279b76fc7a3
SendyAraceli <araceli.arcosm25@gmail.com>
2021-08-11 19:56:04 -0500

    Se agrega el proyecto

---
+++ hola mundo.cpp
@@ -0,0 +1,23 @@
+/* primer programa en C++
+Sendy Araceli Arcos Medrano*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	char nombre[40];
+	
+	cout <<"Hola escribe tu nombre";
+	
+	cout <<endl;
+	
+	cin >>nombre;
+	
+	cout<<"Hola " <<nombre<< " Bienvenid@ a la programacion\n";
+	cout<<"Aqui inicia una nueva aventura";
+	
+	cout <<endl;
+	
+	return 0;
+}
